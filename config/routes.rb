4ad6e63768d198795eebdfe2a08201424997c5a6
mibshara <bsharapc@gmail.com>
2017-11-25 01:56:52 -0600

    Added carts routs

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'shop' => 'pages#shop'
 
+  resources :carts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
