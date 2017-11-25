5e8a2a3e1f33f90426cfb0ffc5381d1f2e53c544
mibshara <bsharapc@gmail.com>
2017-11-25 02:20:39 -0600

    added methods to increment cart with product items

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   get 'shop' => 'pages#shop'
 
   resources :carts
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
